3fea1a96d758487cce0879ac285dd3ce5122164c
Gabriel Sobrinho <gabriel.sobrinho@gmail.com>
2009-12-25 23:30:20 -0200

    Prototype of Projects#Index

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,5 @@
+class ProjectsController < ApplicationController
+  def index
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :members, :only => [:index, :show]
+  map.resources :projects, :only => :index
 
   map.root :controller => 'home', :action => 'index'
 end
