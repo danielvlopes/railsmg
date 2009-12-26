dca2b4c33e4c12786870662bc6625d3ea54a735b
Gabriel Sobrinho <gabriel.sobrinho@gmail.com>
2009-12-26 00:37:59 -0200

    Configure Jammit

--- config/environment.rb
+++ config/environment.rb
@@ -19,6 +19,7 @@ Rails::Initializer.run do |config|
 
   config.gem 'erubis', :version => '2.6.5'
   config.gem 'haml', :version => '2.2.15'
+  config.gem 'jammit', :version => '0.3.2'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
+  # Jammit
+  Jammit::Routes.draw(map)
+
+  # Content
   map.resources :members, :only => [:index, :show]
   map.resources :projects, :meetings, :only => :index
 
+  # Home
   map.root :controller => 'home', :action => 'index'
 end
 
