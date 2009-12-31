939f092ea6f831e6378797450eb430d1eaddf62f
Gabriel Sobrinho <gabriel.sobrinho@gmail.com>
2009-12-30 23:29:05 -0200

    Fix issue with gravatar helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def gravatar_image_tag email, options = {}
     email = Digest::MD5.hexdigest(email)
-    default = request.protocol + request.host_with_port + CGI.escape(image_path('gravatar.jpg'))
+    default = CGI.escape(request.protocol + request.host_with_port + image_path('gravatar.jpg'))
 
     image_tag "http://www.gravatar.com/avatar/#{email}?size=48&default=#{default}", options
   end
