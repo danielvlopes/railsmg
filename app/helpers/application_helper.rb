a0dd6ffb53d2799047eb00303c83eb3e138fda2d
Gabriel Sobrinho <sobrinho@Macintosh.local>
2010-01-05 01:11:24 -0200

    Create helper for navigation links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,5 +5,18 @@ module ApplicationHelper
 
     image_tag "http://www.gravatar.com/avatar/#{email}?size=48&default=#{default}", options
   end
+  
+  def link_to_unless_active name, options = {}, html_options = {}
+    # Members should be active into index and show
+    if options == members_path
+      active = controller.controller_name == 'members'
+    else
+      active = current_page?(options)
+    end
+    
+    link_to_unless(active, name, options, html_options) do
+      link_to name, options, html_options.merge(:class => 'active')
+    end
+  end
 end
 
