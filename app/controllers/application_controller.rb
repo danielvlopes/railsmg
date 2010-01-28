41fcfe62a099b22c9c5a41a9c26a255866b830ca
Daniel Lopes <danielvlopes@gmail.com>
2010-01-27 23:44:16 -0200

    add http basic auth for beta test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,11 @@ class ApplicationController < ActionController::Base
   filter_parameter_logging :password, :password_confirmation
 
   helper_method :current_user, :signed_in?
-  
+
   rescue_from ActiveRecord::RecordNotFound, :with => :not_found
   rescue_from CanCan::AccessDenied, :with => :access_denied
-  
+  # FIXME - remove it for final release
+  before_filter :beta
 protected
 
   def current_user_session
@@ -20,13 +21,19 @@ protected
   def signed_in?
     current_user ? true : false
   end
-  
+
 private
-  
+  # FIXME - remove it for final release
+  def beta
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "beta" && password == YAML.load_file(File.join(RAILS_ROOT, 'tmp', 'pass.yml'))["password"]
+    end
+  end
+
   def access_denied
     render 'static/access_denied', :status => 401
   end
-  
+
   def not_found
     render 'static/not_found', :status => 404
   end
