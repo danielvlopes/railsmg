8b7111a21a205796902e43e3142fe5585817aad5
Joao Vitor <joao.vitor@gmail.com>
2010-01-29 23:34:48 -0200

    rspec updated to 1.3.0, rspec-rails 1.3.2, remarkable_rails 3.1.12

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -26,7 +26,7 @@ config.action_mailer.delivery_method = :test
 # This is necessary if your schema can't be completely dumped by the schema dumper,
 # like if you have constraints or database-specific column types
 # config.active_record.schema_format = :sql
-config.gem 'rspec', :lib => false, :version => '1.2.9'
-config.gem 'rspec-rails', :lib => false, :version => '1.2.9'
-config.gem 'remarkable_rails', :lib => false, :version => '3.1.11'
+config.gem 'rspec', :lib => false, :version => '1.3.0'
+config.gem 'rspec-rails', :lib => false, :version => '1.3.2'
+config.gem 'remarkable_rails', :lib => false, :version => '3.1.12'
 config.gem 'machinist', :lib => false, :version => '1.0.6'
