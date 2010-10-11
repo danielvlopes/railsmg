cfac9adb4713c98c628d9cad40daf9f9eb0709ef
Gabriel Sobrinho <gabriel.sobrinho@gmail.com>
2010-10-11 00:40:36 -0300

    Refactoring and cleanup views
    - Some duplications removed
    - Rails XSS now escapes text of link_to
    - Always write content on next line
    - Avoid unnecessary method calls
    - Fix depecrated syntax on truncate
    - Remove unnecessary ()

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 module ApplicationHelper
+  def link_to_sponsor(name, url)
+    link_to image_tag("sponsors/#{name.downcase}.jpg", :alt => name), url, :popup => true
+  end
+  
+  def link_to_advertise
+    mail_to('contato@railsmg.org', image_tag('sponsors/default.jpg', :alt => 'Anuncie aqui'), :encode => :javascript).html_safe
+  end
+  
   def gravatar_image_tag(email)
     digest = Digest::MD5.hexdigest(email)
     default = CGI.escape(request.protocol + request.host_with_port + image_path('gravatar.jpg'))
