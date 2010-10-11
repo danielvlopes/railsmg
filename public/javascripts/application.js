177684dbacfba4ebca5957dd3b417742f8897e89
Gabriel Sobrinho <gabriel.sobrinho@gmail.com>
2010-10-11 00:19:58 -0300

    We don't need Prototype to support HTML5 on IE6

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,12 +1,11 @@
-if (Prototype.Browser.IE) {
-    (function () {
-        var e = ("abbr,article,aside,audio,canvas,datalist,details," +
-                 "figure,footer,header,hgroup,mark,menu,meter,nav,output," +
-                 "progress,section,time,video").split(',');
+// HTML5 for IE
+// Doesn't affect others browsers
+(function () {
+    var e = ("abbr,article,aside,audio,canvas,datalist,details," +
+             "figure,footer,header,hgroup,mark,menu,meter,nav,output," +
+             "progress,section,time,video").split(',');
 
-        for (var i = 0; i < e.length; i++) {
-            document.createElement(e[i]);
-        }
-    })();
-}
-
+    for (var i = 0; i < e.length; i++) {
+        document.createElement(e[i]);
+    }
+})();
