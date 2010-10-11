2cb865c43d86f4cd7cf260cd24d2c256fdc8698d
Gabriel Sobrinho <gabriel.sobrinho@gmail.com>
2010-10-11 00:22:34 -0300

    Update formtastic initializer to latest version

--- config/initializers/formtastic.rb
+++ config/initializers/formtastic.rb
@@ -1,8 +1,12 @@
 # Set the default text field size when input is a string. Default is 50.
 # Formtastic::SemanticFormBuilder.default_text_field_size = 50
 
+# Set the default text area height when input is a text. Default is 20.
+# Formtastic::SemanticFormBuilder.default_text_area_height = 5
+
 # Should all fields be considered "required" by default?
-# Defaults to true, see ValidationReflection notes below.
+# Rails 2 only, ignored by Rails 3 because it will never fall back to this default.
+# Defaults to true.
 # Formtastic::SemanticFormBuilder.all_fields_required_by_default = true
 
 # Should select fields have a blank option/prompt by default?
@@ -25,7 +29,7 @@
 # Formtastic::SemanticFormBuilder.inline_errors = :sentence
 
 # Set the method to call on label text to transform or format it for human-friendly
-# reading when formtastic is user without object. Defaults to :humanize.
+# reading when formtastic is used without object. Defaults to :humanize.
 # Formtastic::SemanticFormBuilder.label_str_method = :humanize
 
 # Set the array of methods to try calling on parent objects in :select and :radio inputs
