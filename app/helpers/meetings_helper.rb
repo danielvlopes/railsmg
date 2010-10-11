91c4303891f9b8aff27dc0bdc34de07b86be0061
Gabriel Sobrinho <gabriel.sobrinho@gmail.com>
2010-10-11 00:00:31 -0300

    Refactoring on meetings helper

--- app/helpers/meetings_helper.rb
+++ app/helpers/meetings_helper.rb
@@ -1,12 +1,10 @@
 module MeetingsHelper
-  
   def meeting_attachments(meeting)
-    output = []
-    output << link_to("CODE", meeting.code, :popup => true)   if meeting.code.present?
-    output << link_to("VÍDEO", meeting.video, :popup => true) if meeting.video.present?
-    output << link_to("SLIDES", meeting.slides, :popup => true) if meeting.slides.present?
+    attachments = []
+    attachments << link_to("CODE", meeting.code, :popup => true) if meeting.code?
+    attachments << link_to("VÍDEO", meeting.video, :popup => true) if meeting.video?
+    attachments << link_to("SLIDES", meeting.slides, :popup => true) if meeting.slides?
     
-    (output.present?) ? output.join(",").html_safe! : "Não disponível"
+    attachments.empty? ? "Não disponível" : attachments.to_sentence.html_safe
   end
-  
 end
