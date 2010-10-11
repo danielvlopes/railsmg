40dc7e544e96dce10edde30664776a20e71d8ff4
Gabriel Sobrinho <gabriel.sobrinho@gmail.com>
2010-10-11 00:04:51 -0300

    Add () around parameters on method definition

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,7 +1,7 @@
 class Notifier < ActionMailer::Base
   layout "mailer"
   
-  def signup_confirmation user
+  def signup_confirmation(user)
     recipients user.email_with_name
     subject    "#{APP_CONFIG[:name]} - confirmação de cadastro"
     from       APP_CONFIG[:noreply_email]
