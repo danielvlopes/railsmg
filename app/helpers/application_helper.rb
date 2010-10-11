8062ad8ad5c4f6cd6df4c0e2ac9ce6eace5ba634
Gabriel Sobrinho <gabriel.sobrinho@gmail.com>
2010-10-11 00:00:02 -0300

    Refactoring on application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,12 @@
 module ApplicationHelper
-  def gravatar_image_tag email
-    email = Digest::MD5.hexdigest(email)
+  def gravatar_image_tag(email)
+    digest = Digest::MD5.hexdigest(email)
     default = CGI.escape(request.protocol + request.host_with_port + image_path('gravatar.jpg'))
 
-    image_tag "http://www.gravatar.com/avatar/#{email}?size=48&default=#{default}", :alt => 'Gravatar'
+    image_tag("http://www.gravatar.com/avatar/#{digest}?size=48&default=#{default}", :alt => 'Gravatar')
   end
 
   def google_analytics
-    render :partial => 'shared/analytics' if Rails.env == 'production'
+    render 'shared/analytics' if Rails.env.production?
   end
-
 end
